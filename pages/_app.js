81f23cc8dcb1da06234cdfb18c34f568607f697b
Jonathan Rivlin <jonathan@rivlin.dev>
2026-09-04 14:44:57 +0200

    feat: add Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import Footer from '../components/Footer'
+import { Analytics } from '@vercel/analytics/next'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Footer />
+      <Analytics />
     </>
   )
 }
